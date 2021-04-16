297474792adb133c443e223b6320f85aedc648d0
denispaluca <denispaluca@gmail.com>
2021-04-16 22:58:07 +0200

    Add hgToGit function

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 const reader = new FileReader();
-reader.addEventListener('load', (event) => {
-    console.log(event.target.result);
-    var link = document.createElement('a');
-    link.download = '.gitignore';
-    var blob = new Blob([event.target.result], { type: 'text/plain' });
-    link.href = window.URL.createObjectURL(blob);
-    link.click();
+reader.addEventListener('loadend', (event) => {
+  const link = document.createElement('a');
+  link.download = '_.gitignore';
+  const blob = new Blob([hgToGit(event.target.result)], { type: 'text/plain' });
+  link.href = window.URL.createObjectURL(blob);
+  link.click();
 });
 
+function hgToGit(text) {
+  let result = '';
+  let match;
+  let syntax = 'regexp';
+  for (let line of text.split('\n')) {
+    if (line.match(/^\s*$/) || line.match(/^#/)) {
+      result += line + '\n';
+      continue;
+    }
+
+    const regex = new RegExp(String.raw`/^${syntax}\s*:\s*(\S+)/`);
+    if (match = line.match(regex)) {
+      syntax = match[0].toLowerCase();
+      //die "Unknown syntax '$syntax' on line $lineno of $hi\n" if ($syntax !~ /(glob|regexp)/);
+      continue;
+    }
+
+    if (match = line.match(/^((?:\S|\\#)+)(\s*#.*)?\s*$/)) {
+
+      let content = match[0];
+      let comment = match[1];
+
+      if (syntax == 'glob') {
+        result += `${content}${comment}\n`;
+        continue;
+      }
+
+      const newLine = content
+        .replace(/[\^\$]/g, '')
+        .replace(/\.\*/g, '*')
+        .replace(/\./g, '?')
+        .replace(/\\./g, '.')
+        .replace(/(\[.+?\])/g, content);
+
+      result += `${newLine}${comment}\n`;
+    }
+  }
+  return result;
+}
 
 const fileSelector = document.getElementById('file-selector');
 fileSelector.addEventListener('change', (event) => {
-    const fileList = event.target.files;
-    console.log(fileList);
+  const fileList = event.target.files;
 
-    reader.readAsText(fileList[0]);
+  reader.readAsText(fileList[0]);
 });
